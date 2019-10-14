5d09912cc4234b9d26f1441baaf62560e3c9bd33
Jacob Engstrom <jakeengstrom3@gmail.com>
2019-10-14 15:47:07 -0700

    Added jQuery functionality to jQuery.html (Fade out on main header and self destruct button)

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ document.addEventListener("DOMContentLoaded", function() {
       this.style.color = 'orange'
     }
   })
+
+$( document ).ready(function() {
+  $( "#fade" ).click(function() {
+    $( "#fade" ).fadeOut( "slow", function() {
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("button").onclick = function() {
+    $( "#main" ).fadeOut( "slow", function() {
+    });
+  }
+})
+
+$( document ).ready(function() {
+  $( "#IMG" ).click(function() {
+    $( "#IMG" ).fadeOut( "slow", function() {
+    });
+  });
+});
